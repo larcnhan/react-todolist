be7b057cb1b746318f1c0696ccd8ba8d9e840694
hanguozhi <hangz80@126.com>
2019-09-19 09:46:53 +0800

    使用构造函数处理列表

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,6 +1,18 @@
 import React, {Component, Fragment} from 'react'
 
 class TodoList extends Component {
+    /**
+     * 构造函数
+     * @param {*} props 
+     */
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            list: ['study react', 'study englist']
+        }
+    }
+
     render() {
         return (
             <Fragment>
@@ -9,8 +21,7 @@ class TodoList extends Component {
                 <button>添加</button>
             </div>
             <ul>
-                <li>study react</li>
-                <li>study english</li>
+                {this.state.list.map((item, index) => <li>{item}</li>)}
             </ul>
             </Fragment>
         )
